05aaab48e521d5cce0b87c4effc429ec51f98c0a
Lance <lancefinke@icloud.com>
2024-04-21 21:58:39 -0500

    useful math functions

---
+++ Math_Functions.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<cmath>
+
+int main(){
+
+    //max returns max of 2 values
+    //min does same but with min value
+    int z = std::max(3,4);//z=4
+
+    //exponents:
+    int x = pow(4,2);//x=16
+
+    //square root
+    int y = sqrt(64);//y=8
+
+    //absolute value
+    int ab = abs(-3);//ab=3
+
+    //round
+    int w = round(4.323);//w=4
+
+    //ceil
+    int c =ceil(5.1);//c=6
+
+    //floor
+    int f = floor(5.99);//f=5
+
+    //useful math functions:  www.cplusplus.com/reference/cmath/
+
+
+
+    return 0;
+}
