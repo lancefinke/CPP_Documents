71be7da23f8be3ac110307802f94ed145af867d1
Lance <lancefinke@icloud.com>
2024-04-21 21:25:42 -0500

    typedef,arithmetic, and using files

---
+++ Typedef_And_Using.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+
+//typedef std::vector<std::pair<std::string, int>> pairlist_t;
+//typedef std::string text_t;
+
+//typedef has mostly been replaced with using:
+using text_t = std::string;
+
+int main(){
+    
+    /*pairlist creates new identitfier for datatype
+    pairlist_t pairlist;*/
+
+    text_t name="Lance";//behaves like string
+
+    std::cout<<name<<std::endl;
+
+    return 0;
+}
