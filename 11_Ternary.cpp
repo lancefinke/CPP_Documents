96bcf9077862d8e8834d0fdb92235cd8193874a6
Lance <lancefinke@icloud.com>
2024-04-22 21:57:56 -0500

    ternary

---
+++ 11_Ternary.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    //same as javascript,java,etc
+    int grade;
+    cout<<"Enter your Test Grade: \n";
+
+    cin>>grade;
+
+    grade>=70 ? cout<<"YOU PASSED!" : cout<<"YOU FAILED";
+
+
+
+    return 0;
+}
