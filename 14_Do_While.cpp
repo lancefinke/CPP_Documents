80188af65cd975bea6f0152b5ddc7cbcabad84f5
Lance <lancefinke@icloud.com>
2024-04-23 20:52:11 -0500

    Do/while

---
+++ 14_Do_While.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    int number;
+
+    do{
+        cout<<"Enter a positive Number: ";
+        cin>>number;
+    }while(number<0);
+
+    cout<< "The Number is zero";
+
+    return 0;
+}
